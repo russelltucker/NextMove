ceba65ca3525c7317bc76d54d3cb3095c934dbe7
russelltucker <russell.tucker@customprofiles.com>
2026-08-19 15:15:14 -0400

    Refresh PWA cache for dashboard project visibility fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='nextmove-sync-v2.0.1-dashboardfix';
+const CACHE='nextmove-sync-v2.0.1-dashboardfix2';
 const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','icons/nextmove-icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
